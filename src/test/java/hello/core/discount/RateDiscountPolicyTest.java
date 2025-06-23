a7b8b4d86fc0e0c928de08f873268d088c2a686d
chanition <chanyeong1206@gmail.com>
2025-06-23 15:34:38 +0900

    SpringStudy : ~rateDiscountPolicy

---
+++ src/test/java/hello/core/discount/RateDiscountPolicyTest.java
@@ -0,0 +1,10 @@
+package hello.core.discount;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RateDiscountPolicyTest{
+
+    RateDiscountPolicy discountPolicy = new RateDiscountPolicy();
+
+
+}
