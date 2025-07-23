d998f664be12c0ec6f22ae1c541050ac99edb197
chanition <chanyeong1206@gmail.com>
2025-07-23 16:42:11 +0900

    ~singletonTest

--- src/test/java/hello/core/singleton/singletonTest.java
+++ src/test/java/hello/core/singleton/singletonTest.java
@@ -2,6 +2,7 @@ package hello.core.singleton;
 
 import hello.core.AppConfig;
 import hello.core.member.MemberService;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -18,5 +19,8 @@ public class singletonTest {
         //참조값이 다른 것을 확인
         System.out.println("memberService1: " + memberSevice1);
         System.out.println("memberService2: " + memberSevice2);
+
+        //memberService 1 은 memberService랑 다름
+        Assertions.assertThat(memberSevice1).isNotSameAs(memberSevice2);
     }
 }
